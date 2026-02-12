c8388bce49f088067be281b5d791770dee33ef7f
中島浩二 <knaka0209@gmail.com>
2026-02-12 09:59:04 +0900

    up, 0212a3

--- rag_18/main.py
+++ rag_18/main.py
@@ -1,7 +1,8 @@
 import asyncio
+import duckdb
 import os
 import ollama
-import duckdb
+import uuid
 from copilot import CopilotClient
 from fastapi import FastAPI, Request
 from fastapi import FastAPI, Request, Depends, HTTPException
@@ -11,14 +12,21 @@ from fastapi.templating import Jinja2Templates
 import json
 import requests
 import sys
+
 # Pydanticモデル（スキーマ）のインポート
 from pydantic import BaseModel
 
 class SearchRequest(BaseModel):
     query: str
 
+class AddRequest(BaseModel):
+    content: str
+
+class EditDeleteRequest(BaseModel):
+    id: str
+
 app = FastAPI()
-DB_FILE="/tmp/vector.db"
+DB_FILE="vector.db"
 
 # public フォルダを静的ファイルとしてマウント
 # /static というURLパスで public フォルダの内容にアクセスできるようになります。
@@ -101,8 +109,115 @@ async def rag_search(item: SearchRequest):
 #
 #
 #
+@app.post("/api/edit_list")
+async def edit_list(item: SearchRequest):
+    conn = duckdb.connect(database=DB_FILE)
+    conn.execute("INSTALL vss; LOAD vss;")
+    print("query=" + item.query)
+    query = item.query
+
+    print("query=" + query)
+    if query == "":
+        select_sql = """SELECT 
+        id, 
+        text, 
+        FROM embeddings
+        ORDER BY id DESC
+        LIMIT 100;
+        """
+    else:
+        select_sql = """SELECT id, text 
+        FROM embeddings
+        """
+        select_sql += f" WHERE text LIKE '%{query}%' LIMIT 100"
+    print("select_sql=" + select_sql)
+
+    resp = conn.execute(select_sql).fetchall()
+    matches = ""
+    ouStr = "" 
+    #print(resp)
+
+    return {
+        "status": "success",
+        "result": resp
+    }
+
+#
+#
+#
+@app.post("/api/edit_create")
+async def edit_create(item: AddRequest):
+    conn = duckdb.connect(database=DB_FILE)
+    conn.execute("INSTALL vss; LOAD vss;")
+    print("query=" + item.content)
+    in_text = item.content
+
+    print("in_text=" + in_text)
+    
+    embedding = ollama.embeddings(
+        model="qwen3-embedding:0.6b",
+        prompt=in_text
+    )
+    vec = embedding["embedding"]
+
+    print(len(vec))
+    print(vec[:5])   
+
+    add_vector = embedding["embedding"]
+    newUID = str(uuid.uuid4())
+    insert_sql = """
+    INSERT INTO embeddings 
+        (id, text, vector) VALUES 
+        (?, ?, CAST(? AS FLOAT[1024]))
+    """
+    conn.execute(insert_sql , (newUID , in_text, add_vector))
+    return {
+        "status": "success",
+        "result": ""
+    }  
+
+#
+#
+#
+@app.post("/api/edit_delete")
+async def edit_delete(item: EditDeleteRequest):
+    conn = duckdb.connect(database=DB_FILE)
+    conn.execute("INSTALL vss; LOAD vss;")
+    print("id=" + item.id)
+    id = item.id
+
+    delete_sql = """
+    DELETE FROM embeddings WHERE id = $1
+    """
+    conn.execute(delete_sql , [id])
+
+    return {
+        "status": "success",
+        "result": ""
+    }  
+
+#
+#
+#
 @app.get("/", response_class=HTMLResponse)
 async def home_page():
+    return """
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>FastAPI Static Files</title>
+        <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+        <link href="/static/main.css" rel="stylesheet"/>
+    </head>
+    <body>
+        <div id="app"></div>
+        <script type="module" src="/static/client.js"></script>
+    </body>
+    </html>
+    """
+
+@app.get("/edit", response_class=HTMLResponse)
+async def edit_page():
     return """
     <!DOCTYPE html>
     <html>
