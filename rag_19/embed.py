a2f6cd1248e6a95e8b2373a6866bbe5f2d1e28a6
中島浩二 <knaka0209@gmail.com>
2026-02-13 12:38:20 +0900

    up, 0213a7

--- rag_19/embed.py
+++ rag_19/embed.py
@@ -9,7 +9,7 @@ folder_path = "./data"
 collection_name = "my_document_collection"
 # スプリッターの初期化
 text_splitter = RecursiveCharacterTextSplitter(
-    chunk_size=500,      # 1チャンクあたりの文字数（目安）
+    chunk_size=1000,      # 1チャンクあたりの文字数（目安）
     chunk_overlap=100,    # 前後のチャンクと重複させる文字数（文脈維持のため）
     length_function=len, # 長さを測る関数（通常はlen）
     separators=["\n\n", "\n", "。", " ", ""] # 分割する優先順位
@@ -22,18 +22,22 @@ text_splitter = RecursiveCharacterTextSplitter(
 def read_file(collection):
     data = []
     contData = []
-    for file_path in glob.glob(os.path.join(folder_path, "*.txt")):
-        with open(file_path, "r", encoding="utf-8") as f:
-            targetContent = f.read()
-            # 分割実行
-            chunks = text_splitter.create_documents([targetContent])
-            for i, chunk in enumerate(chunks):
-                print(f"Chunk {i}: {chunk.page_content}")
-                data.append({
-                    "filename": os.path.basename(file_path),
-                    "content": chunk.page_content,
-                })
-                contData.append(chunk.page_content)
+    for file_path in glob.glob(os.path.join(folder_path, "*.*")):
+        root, extension = os.path.splitext(file_path)
+        print("ext:"+ extension)
+        if extension.lower() == ".txt" or extension.lower() == ".md":
+            with open(file_path, "r", encoding="utf-8") as f:
+                targetContent = f.read()
+                # 分割実行
+                chunks = text_splitter.create_documents([targetContent])
+                for i, chunk in enumerate(chunks):
+                    print(f"Chunk {i}: {chunk.page_content}")
+                    data.append({
+                        "filename": os.path.basename(file_path),
+                        "content": chunk.page_content,
+                    })
+                    contData.append(chunk.page_content)
+
 
     #
     for i, d in enumerate(data):
